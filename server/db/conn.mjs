011494f87b5f9138279361638b6b9c3ba72de746
eedra <midirilorenzo@gmail.com>
2024-05-05 02:00:09 +0200

    Revert "ciao"
    This reverts commit caa2047d7657d971125c93ec54f4895b30662c24.

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -5,13 +5,10 @@ const connectionString = process.env.ATLAS_URI || "";
 const client = new MongoClient(connectionString);
 
 let conn;
-try 
-{
-	conn = await client.connect();
-} 
-catch(e) 
-{
-	console.error(e);
+try {
+  conn = await client.connect();
+} catch(e) {
+  console.error(e);
 }
 
 let db = conn.db("TrenGo");
--- server/index.mjs
+++ server/index.mjs
@@ -4,10 +4,7 @@ import "./loadEnvironment.mjs";
 import "express-async-errors";
 import utente from "./routes/utente.mjs";
 import proposta from "./routes/proposta.mjs";
-<<<<<<< Updated upstream
 //import swaggerjsdoc from "swagger-jsdoc";
-=======
->>>>>>> Stashed changes
 import swaggerui from "swagger-ui-express";
 import YAML from "yamljs";
 const swaggerFile = YAML.load('./swagger.yml');
@@ -16,8 +13,8 @@ const swaggerFile = YAML.load('./swagger.yml');
 const PORT = process.env.PORT || 5050;
 const app = express();
 app.use(
-	"/api-docs", 
-	swaggerui.serve, 
+  "/api-docs", 
+  swaggerui.serve, 
   swaggerui.setup(swaggerFile)
 );
 
@@ -30,13 +27,11 @@ app.use("/utente", utente);
 app.use("/proposta", proposta);
 
 // Global error handling
-app.use((err, _req, res) =>
-{
-	res.status(500).send("Uh oh! An unexpected error occured.")
+app.use((err, _req, res, next) => {
+  res.status(500).send("Uh oh! An unexpected error occured.")
 })
 
 // start the Express server
-app.listen(PORT, () => 
-{
-	console.log(`Server is running on port: ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
 });
