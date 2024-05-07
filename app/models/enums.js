1f544f6c1b562457ed6dff2da01eddf7f2fdfa7a
eedra <122636587+eedra@users.noreply.github.com>
2024-05-07 21:12:20 +0200

    Aggiunti modelli per proposta, utente e enum

---
+++ app/models/enums.js
@@ -0,0 +1,29 @@
+const interessiEnum= 
+[
+    'Reading',
+    'Running',
+    'Cooking',
+    'Photography',
+    'Gaming',
+    'Painting',
+    'Traveling',
+    'Hiking',
+    'Cycling',
+    'Writing',
+    'Singing',
+    'Dancing',
+    'Fishing',
+    'Swimming',
+    'Yoga',
+    'Gardening',
+    'Watching Movies',
+    'Playing Musical Instruments',
+    'Birdwatching',
+    'Collecting',
+    'Other'
+];
+
+module.exports= 
+{
+    interessiEnum
+};
