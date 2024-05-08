5b6edf7edd5c605d006d525c450f125a79384c0a
Roberto <roberto.savi2582@gmail.com>
2024-05-08 19:58:20 +0200

    Create API utente (handlers, routes, validators aggiiornati)

--- server/routes/utentiRoutes.mjs
+++ server/routes/utentiRoutes.mjs
@@ -1,47 +1,24 @@
 import express from "express";
-import db from "../db/conn.mjs";
-import { ObjectId } from "mongodb";
+import {getUtenteById, updateUtenteById, getUtenteByUsername, signupUtente, loginUtente} from "../handlers/utentiHandlers.mjs"
+
 
 const router = express.Router();
 
+// Permette di ottenere i dati di un utente tramite l'id
+router.get("/:id", getUtenteById);
+
+// Permette di modificare un utente tramite l'id
+router.put("/:id", updateUtenteById);
+
+// Permette di di ottenere i dati di un utente tramite l'username
+router.get("/:username", getUtenteByUsername);
+
 // Permette la registrazione di un utente
-router.post("/signup",utentiSignup);
+router.post("/signup", signupUtente);
 
 // Permette il login di un utente
-router.post("/signup",utentiLogin);
+router.post("/login", loginUtente);
 
 
-
-// Get a single post
-router.get("/:id", async (req, res) => {
-  let collection = await db.collection("posts");
-  let query = {_id: ObjectId(req.params.id)};
-  let result = await collection.findOne(query);
-
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
-});
-
-// Aggiungi un nuovo utente alla collection utenti
-router.post("/", async (req, res) => {
-  let collection = await db.collection("Utenti");
-  let newDocument = req.body;
-  newDocument.date = new Date();
-  let result = await collection.insertOne(newDocument);
-  res.send(result).status(204);
-});
-
-// Update the post with a new comment
-router.patch("/comment/:id", async (req, res) => {
-  const query = { _id: ObjectId(req.params.id) };
-  const updates = {
-    $push: { comments: req.body }
-  };
-
-  let collection = await db.collection("posts");
-  let result = await collection.updateOne(query, updates);
-
-  res.send(result).status(200);
-});
-
-export default router;
+// Esporta router
+module.exports = router;
