c735659e9942f03434675c658837d3137f6b0286
eedra <midirilorenzo@gmail.com>
2024-06-10 17:17:14 +0200

    Configurazione vite sistemata

--- app/vite.config.mjs
+++ app/vite.config.mjs
@@ -4,14 +4,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  // base: process.env.BASE_PATH || '/'
-  base: process.env.NODE_ENV === 'production'
-          ? '/Trengo/' // prod
-          : '/', // dev
+	plugins: [vue()],
+	server: {
+		port: import.meta.env.PORT, 
+		open: true, 
+	},
+	resolve: {
+		alias: {
+			'@': '/src'
+		}
+	}
 })
