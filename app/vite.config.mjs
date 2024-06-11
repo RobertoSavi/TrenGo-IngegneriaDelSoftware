f4a3459a67c7dcd3f5686c11bec2e596dcd372aa
Roberto <roberto.savi2582@gmail.com>
2024-06-11 23:07:02 +0200

    Update vite.config.mjs

--- app/vite.config.mjs
+++ app/vite.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
 	server: {
 		host: true,
-		port: process.env.VITE_PORT, 
+		port: 3000, 
 		protocol: "ws",
 	},
 	resolve: {
