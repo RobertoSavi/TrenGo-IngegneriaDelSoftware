79702cf20af7ae75b903563f5e9e6c4185b891d4
eedra <midirilorenzo@gmail.com>
2024-06-11 21:46:49 +0200

    Risoluzione vite config

--- app/vite.config.mjs
+++ app/vite.config.mjs
@@ -6,9 +6,8 @@ import dotenv from 'dotenv';
 export default defineConfig({
 	plugins: [vue()],
 	server: {
-		host: process.env.VITE_HOST,
+		host: true,
 		port: process.env.VITE_PORT, 
-		open: true, 
 	},
 	resolve: {
 		alias: {
