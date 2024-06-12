5facbb3f56c446e5648e5ebd6be60a872a340803
Roberto <roberto.savi2582@gmail.com>
2024-06-12 16:35:30 +0200

    Update vite.config.mjs

--- app/vite.config.mjs
+++ app/vite.config.mjs
@@ -5,8 +5,10 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
 	plugins: [vue()],
 	server: {
-	  host: true, // or true for external access
-	  port: 3000,
+		hmr:{
+			host: true, // or true for external access
+			Port: 3000,
+		}
 	},
 	resolve: {
 	  alias: {
