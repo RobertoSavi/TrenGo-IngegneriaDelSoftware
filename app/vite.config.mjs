d4d65203f3e16cc88489675b306da61013c87204
eedra <midirilorenzo@gmail.com>
2024-06-12 17:20:58 +0200

    Update vite.config.mjs

--- app/vite.config.mjs
+++ app/vite.config.mjs
@@ -5,14 +5,17 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
 	plugins: [vue()],
 	server: {
-		Port: 3000,
-		hmr:{
-			Port: 3000,
-		}
+		host: true,
+		port: process.env.VITE_PORT||3000,
+		hmr: {
+			protocol: isDevelopment ? 'ws' : 'wss', // Use secure WebSockets in production
+			host: isDevelopment ? 'localhost' : undefined, // Don't set the host in production
+			port: isDevelopment ? 3000 : undefined // Don't set the port in production
+		},
 	},
 	resolve: {
-	  alias: {
-		'@': '/src'
-	  }
+		alias: {
+			'@': '/src'
+		}
 	}
-  });
+});
