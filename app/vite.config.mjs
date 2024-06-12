f1b3378b48c7c2c78244c82721aea4207a0c6556
eedra <midirilorenzo@gmail.com>
2024-06-12 17:41:42 +0200

    Permessi con wss

--- app/vite.config.mjs
+++ app/vite.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-const isDevelopment = process.env.NODE_ENV === 'development';
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue()],
+	plugins: [vue(), mkcert()],
 	server: {
+		http: true,
+		https: true,
 		host: true,
 		port: process.env.VITE_PORT||3000,
 		hmr: {
-			protocol: 'ws', // Use secure WebSockets in production
+			protocol: 'wss', // Use secure WebSockets in production
 		},
 	},
 	resolve: {
